5105ea5b1108b3410aa54fd7f7c5d4cccfacd383
ugur <ugur@skenariolabs.com>
2024-05-09 00:52:08 +0300

    first commit

---
+++ src/test/java/com/cydeo/tests/week001/HW002_CartPage.java
@@ -0,0 +1,15 @@
+package com.cydeo.tests.week001;
+
+import com.cydeo.tests.base.AutomationExerciseTestBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HW002_CartPage extends AutomationExerciseTestBase {
+    @Test
+    void verifyEmptyCart() {
+        page.navigate("/view_cart");
+        String actualLabel = page.locator("//p/b").textContent();
+        Assertions.assertEquals("Cart is empty!",actualLabel);
+
+    }
+}
